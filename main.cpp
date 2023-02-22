61915ea6c15557ab3e9a58d8bf5e8cb616a3bc5b
Zaine <74148219+rhinorunner@users.noreply.github.com>
2023-02-22 08:42:03 -0500

    fixed everything
    - last number no longer appears no matter what
    - piped output works
    - added some slight optimization

--- main.cpp
+++ main.cpp
@@ -6,11 +6,14 @@
 /********************************************/
 
 // prime ceiling
-constexpr uint64_t PS_CEIL = 25;
+constexpr uint64_t PS_CEIL = 1000;
 
 // pipe output
-// CURRENTLY DOES NOT WORK, KEEP TO FALSE
-constexpr bool PS_PIPED = false;
+constexpr bool PS_PIPED = true;
+
+// seperator between printed values
+// change to std::string if you want
+constexpr char PS_SEP = '\n';
 
 /********************************************/
 
@@ -22,7 +25,7 @@ std::vector<uint64_t> getPrimes(const uint64_t& ceiling)
 	uint64_t maxToAdd = (uint64_t) sqrt(ceiling);
 	
 	primes.push_back({1,false});
-	for (uint64_t i = 2; i <= ceiling; i++) primes.push_back({i,true});
+	for (uint64_t i = 2; i < ceiling; i++) primes.push_back({i,true});
 
 	for (auto i : primes) {
 		// if number is prime
@@ -42,14 +45,14 @@ std::vector<uint64_t> getPrimes(const uint64_t& ceiling)
 
 int main()
 {
-	if (PS_PIPED) {
-		// pipe output to output.txt
-		std::ofstream out("output.txt");
-		std::streambuf* coutbuf = std::cout.rdbuf();
-		std::cout.rdbuf(out.rdbuf());
-	}
+	std::cout << "processing...\n";
 	auto primes = getPrimes(PS_CEIL);
-	for (uint64_t i : primes) std::cout << i << " ";
+	if (PS_PIPED) {
+		std::ofstream out("output.txt");
+		for (uint64_t& i : primes) out << i << PS_SEP;
+		out.close();
+	}
+	else for (uint64_t& i : primes) std::cout << i << PS_SEP;
 
 	return 0;
 }
