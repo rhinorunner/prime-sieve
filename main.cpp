b3db66d95a6e3ec6add828866d1335c42f93d794
Zaine <74148219+rhinorunner@users.noreply.github.com>
2023-02-22 09:20:57 -0500

    custom integer data type
    keep it at uint32_t unless youre mentally insane and need a number higher than like 4.3 billion

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@
 
 /********************************************/
 
+// data type, idk make it uint64_t if youre somehow using an absurdly high number
+#define PS_DATATYPE uint32_t
+
 // prime ceiling
-static constexpr uint64_t PS_CEIL = 1000000;
+static constexpr PS_DATATYPE PS_CEIL = 100000000;
 
 // pipe output
 static constexpr bool PS_PIPED = true;
@@ -18,15 +21,15 @@ static constexpr char PS_SEP = '\n';
 
 /********************************************/
 
-std::vector<uint64_t> getPrimes(const uint64_t& ceiling)
+std::vector<PS_DATATYPE> getPrimes(const PS_DATATYPE& ceiling)
 {
-	std::vector<std::pair<uint64_t, bool>> primes;
-	std::vector<uint64_t> primesReturn;
+	std::vector<std::pair<PS_DATATYPE, bool>> primes;
+	std::vector<PS_DATATYPE> primesReturn;
 	// any number past this doesnt need to be processed in prime loop
-	uint64_t maxToAdd = (uint64_t) sqrt(ceiling);
+	uint64_t maxToAdd = (PS_DATATYPE) sqrt(ceiling);
 	
 	primes.push_back({1,false});
-	for (uint64_t i = 2; i < ceiling; i++) primes.push_back({i,true});
+	for (PS_DATATYPE i = 2; i < ceiling; i++) primes.push_back({i,true});
 
 	for (auto i : primes) {
 		// if number is prime
@@ -34,7 +37,7 @@ std::vector<uint64_t> getPrimes(const uint64_t& ceiling)
 			primesReturn.push_back(i.first);
 
 			if (i.first < maxToAdd) {
-				for (uint64_t x = (i.first * 2); x <= ceiling; x += i.first) {
+				for (PS_DATATYPE x = (i.first * 2); x <= ceiling; x += i.first) {
 					if (x > ceiling) break;
 					primes[x-1].second = false;
 				}
@@ -56,10 +59,10 @@ int main()
 	const auto TP1w = std::chrono::high_resolution_clock::now();
 	if (PS_PIPED) {
 		std::ofstream out("output.txt");
-		for (uint64_t& i : primes) out << i << PS_SEP;
+		for (PS_DATATYPE& i : primes) out << i << PS_SEP;
 		out.close();
 	}
-	else for (uint64_t& i : primes) std::cout << i << PS_SEP;
+	else for (PS_DATATYPE& i : primes) std::cout << i << PS_SEP;
 	const auto TP2w = std::chrono::high_resolution_clock::now();
 	std::cout << "done.\n";
 
